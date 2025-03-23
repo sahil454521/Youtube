ec10fda78b1b60060ee9710af331a2507baac573
Sahil Pathak <sahilpathak2005@gmail.com>
2025-03-23 22:43:52 +0530

    navbarfinal

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,35 +3,44 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { FaYoutube } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 import { FaMicrophone } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center p-4  bg-[#212121] opacity-98 sticky top-0 z-50 text-white'>
+    <div className='flex justify-between items-center p-4 bg-[#212121] opacity-98 sticky top-0 z-50 text-white'>
       <div className='flex items-center gap-4'>
         <div>
-        <GiHamburgerMenu/>
+          <GiHamburgerMenu />
         </div>
         <div className='flex items-center gap-2 justify-center'>
-        <FaYoutube className='text-2xl text-red-500'/>
-        <span className='text-xl font-bold'>YouTube</span>
+          <FaYoutube className='text-2xl text-red-500' />
+          <span className='text-xl font-bold'>YouTube</span>
         </div>
-        <div className='flex items-center gap-5 pr-5'>
-        <form>
-            <div className='flex items-center gap-3'>
-            
-                <div className='flex items-center '>
-                <input type='text' placeholder='Search' className='w-[500px] bg-[#121212] px-4 py-2 rounded-l-full focus:outline-none border border-[#333]'/>
-                <button className='px-6 py-3 bg-[#333] rounded-r-full border border-l-0 border-[#333] hover:bg-[#444]'>
-                <FaSearch className='text-gray-400'/>
-                </button>
-                <div className='flex items-center gap-8'>
-                <button className='px-6 py-3 bg-[#333] rounded-full border border-[#333] hover:bg-[#444]'>
-                <FaMicrophone className='gap-3 text-gray-400'/>
-                </button>
-                </div>
-                </div>
+      </div>
+      <div className='flex-1 flex justify-center items-center max-w-[800px]'>
+        <form className='w-full'>
+          <div className='flex items-center justify-center gap-3'>
+            <div className='flex items-center w-full'>
+              <input type='text' placeholder='Search' className='w-full max-w-[600px] bg-[#121212] px-4 py-2 rounded-l-full focus:outline-none border border-[#333]' />
+              <button className='px-6 py-3 bg-[#333] rounded-r-full border border-l-0 border-[#333] hover:bg-[#444]'>
+                <FaSearch className='text-gray-400' />
+              </button>
             </div>
+            <div className='flex items-center'>
+              <button className='px-3 py-3 bg-[#333] rounded-full border border-[#333] hover:bg-[#444]'>
+                <FaMicrophone className='text-gray-300' />
+              </button>
+            </div>
+          </div>
         </form>
+      </div>
+      <div className='flex items-center gap-4'>
+        <div className='items-center flex gap-3 px-6 py-2 bg-[#333] rounded-full border border-[#333] hover:bg-[#444] text-white-300'>
+          <FaPlus className='flex items-center' /><span className='font-bold'>Create</span>
+        </div>
+        <div>
+          <IoMdNotificationsOutline className='font-bold text-3xl' />
         </div>
       </div>
     </div>

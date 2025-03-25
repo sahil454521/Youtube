8172115c3ca29a247b040e34fcd25f57ca6c4be9
Sahil Pathak <sahilpathak2005@gmail.com>
2025-03-25 18:21:04 +0530

    Updated useApp hooks for Redux

---
+++ src/store/configurestore.js
@@ -0,0 +1,11 @@
+import {configureStore} from "@reduxjs/toolkit";
+import youtubeReducer from "../features/youtube/youtubeSlice";
+
+const store = configureStore({
+    reducer:{
+        youtubeApp: youtubeReducer,
+    }
+});
+
+
+export default store;
